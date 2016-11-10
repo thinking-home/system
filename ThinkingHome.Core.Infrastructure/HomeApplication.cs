fc3944cd5d25ece7019a15370115d32ebb733aa3
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-11-10 21:38:21 +0300

    logger && plugin base

--- ThinkingHome.Core.Infrastructure/HomeApplication.cs
+++ ThinkingHome.Core.Infrastructure/HomeApplication.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog;
 
 namespace ThinkingHome.Core.Infrastructure
 {
     public class HomeApplication
     {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public void Init()
         {
-            Console.WriteLine("init!");
+            logger.Info("init application!!!!");
         }
     }
 }
---
+++ ThinkingHome.Core.Plugins/PluginBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+
+namespace ThinkingHome.Core.Plugins
+{
+    public class PluginBase
+    {
+        private readonly Logger logger;
+
+        #region life cycle
+
+        protected PluginBase()
+        {
+            logger = LogManager.GetLogger(GetType().FullName);
+        }
+
+        public virtual void InitPlugin()
+        {
+            logger.Warn("init plugin");
+        }
+
+        public virtual void StartPlugin()
+        {
+            logger.Warn("start plugin");
+        }
+
+        public virtual void StopPlugin()
+        {
+            logger.Warn("stop plugin");
+        }
+
+        #endregion
+    }
+}
