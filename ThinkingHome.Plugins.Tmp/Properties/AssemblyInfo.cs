c91b31517ea538d96c8bebe5bcc91bfa3c53b5ae
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-11-12 02:11:20 +0300

    tmp plugin

---
+++ ThinkingHome.Plugins.Tmp/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("ThinkingHome.Plugins.Tmp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThinkingHome.Plugins.Tmp")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("7C2B89B1-104B-4CC1-8907-BD9A4D0E7039")]
---
+++ ThinkingHome.Plugins.Tmp/TmpPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.Tmp
+{
+    public class TmpPlugin : PluginBase
+    {
+        public override void InitPlugin()
+        {
+            Logger.Info("init tmp plugin {0}", Guid.NewGuid());
+        }
+
+        public override void StartPlugin()
+        {
+            Logger.Warn("start tmp plugin {0}", Guid.NewGuid());
+        }
+
+        public override void StopPlugin()
+        {
+            Logger.Debug("stop tmp plugin {0}", Guid.NewGuid());
+        }
+    }
+}
