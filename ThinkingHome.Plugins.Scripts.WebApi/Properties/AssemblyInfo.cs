2fbdb07ce150051874ef7cbf1867a488acbe4d0a
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-01-19 08:52:07 +0300

    script http api project

---
+++ ThinkingHome.Plugins.Scripts.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("ThinkingHome.Plugins.Scripts.WebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThinkingHome.Plugins.Scripts.WebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("D51509B5-8FEA-4577-9001-CABE822CA1C6")]
---
+++ ThinkingHome.Plugins.Scripts.WebApi/ScriptsWebApiPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using ThinkingHome.Core.Plugins;
+using ThinkingHome.Plugins.Database;
+using ThinkingHome.Plugins.Scripts.Model;
+using ThinkingHome.Plugins.WebServer.Attributes;
+using ThinkingHome.Plugins.WebServer.Handlers;
+
+namespace ThinkingHome.Plugins.Scripts.WebApi
+{
+    public class ScriptsWebApiPlugin : PluginBase
+    {
+        [HttpCommand("/api/scripts/list")]
+        public object GetScriptList(HttpRequestParams request)
+        {
+            using (var session = Context.Require<DatabasePlugin>().OpenSession())
+            {
+                var list = session.Set<UserScript>()
+                    .Select(x => new { id = x.Id, name = x.Name })
+                    .ToArray();
+
+                return list;
+            }
+        }
+    }
+}
