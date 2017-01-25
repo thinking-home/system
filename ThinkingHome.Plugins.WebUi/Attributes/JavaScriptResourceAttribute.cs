6e5dd3cd57715d45cd4730e892c8bbdad5219f51
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-01-25 23:33:08 +0300

    system-js & js aliases

--- ThinkingHome.Plugins.WebUi/Attributes/JavaScriptResourceAttribute.cs
+++ ThinkingHome.Plugins.WebUi/Attributes/JavaScriptResourceAttribute.cs
@@ -4,6 +4,8 @@ namespace ThinkingHome.Plugins.WebUi.Attributes
 {
     public class JavaScriptResourceAttribute : HttpEmbeddedResourceAttribute
     {
+        public string Alias { get; set; }
+
         public JavaScriptResourceAttribute(string url, string resourcePath)
             : base(url, resourcePath, "application/javascript")
         {
--- ThinkingHome.Plugins.WebUi/WebUiPlugin.cs
+++ ThinkingHome.Plugins.WebUi/WebUiPlugin.cs
@@ -1,4 +1,7 @@
-﻿using ThinkingHome.Core.Plugins;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using ThinkingHome.Core.Plugins;
+using ThinkingHome.Core.Plugins.Utils;
 using ThinkingHome.Plugins.WebServer.Attributes;
 using ThinkingHome.Plugins.WebServer.Handlers;
 using ThinkingHome.Plugins.WebUi.Attributes;
@@ -9,28 +12,49 @@ namespace ThinkingHome.Plugins.WebUi
     [HttpEmbeddedResource("/", "ThinkingHome.Plugins.WebUi.Resources.Application.index.html", "text/html")]
     [HttpEmbeddedResource("/favicon.ico", "ThinkingHome.Plugins.WebUi.Resources.Application.favicon.ico", "image/x-icon")]
     [JavaScriptResource("/webapp/index.js", "ThinkingHome.Plugins.WebUi.Resources.Application.index.js")]
-    [JavaScriptResource("/webapp/lib.js", "ThinkingHome.Plugins.WebUi.Resources.Application.lib.js")]
+    [JavaScriptResource("/webapp/lib.js", "ThinkingHome.Plugins.WebUi.Resources.Application.lib.js", Alias = "lib")]
     [JavaScriptResource("/webapp/core/app.js", "ThinkingHome.Plugins.WebUi.Resources.Application.core.app.js")]
 
     // vendor
-    [JavaScriptResource("/vendor/js/json2.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.json2.min.js")]
-    [JavaScriptResource("/vendor/js/jquery.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.jquery.min.js")]
+    [JavaScriptResource("/vendor/js/system.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.system.min.js")]
+
+    [JavaScriptResource("/vendor/js/json2.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.json2.min.js", Alias = "json2")]
+    [JavaScriptResource("/vendor/js/jquery.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.jquery.min.js", Alias = "jquery")]
     [JavaScriptResource("/vendor/js/require.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.require.min.js")]
     [JavaScriptResource("/vendor/js/require-text.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.require-text.min.js")]
     [JavaScriptResource("/vendor/js/require-json.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.require-json.min.js")]
-    [JavaScriptResource("/vendor/js/underscore.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.underscore.min.js")]
-    [JavaScriptResource("/vendor/js/backbone.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.backbone.min.js")]
-    [JavaScriptResource("/vendor/js/backbone.radio.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.backbone.radio.min.js")]
-    [JavaScriptResource("/vendor/js/marionette.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.marionette.min.js")]
+    [JavaScriptResource("/vendor/js/underscore.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.underscore.min.js", Alias = "underscore")]
+    [JavaScriptResource("/vendor/js/backbone.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.backbone.min.js", Alias = "backbone")]
+    [JavaScriptResource("/vendor/js/backbone.radio.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.backbone.radio.min.js", Alias = "backbone.radio")]
+    [JavaScriptResource("/vendor/js/marionette.js", "ThinkingHome.Plugins.WebUi.Resources.Vendor.js.marionette.min.js", Alias = "marionette")]
     // [HttpEmbeddedResource("/vendor/css/uikit.min.css", "ThinkingHome.Plugins.WebUi.Resources.Vendor.css.uikit.min.css", "text/css")]
     public class WebUiPlugin : PluginBase
     {
+        private readonly InternalDictionary<string> aliases = new InternalDictionary<string>();
+
+        public override void InitPlugin(IConfigurationSection config)
+        {
+            foreach (var plugin in Context.GetAllPlugins())
+            {
+                var type = plugin.GetType().GetTypeInfo();
+
+                foreach (var jsinfo in type.GetCustomAttributes<JavaScriptResourceAttribute>())
+                {
+                    if (!string.IsNullOrEmpty(jsinfo.Alias))
+                    {
+                        aliases.Register(jsinfo.Alias, jsinfo.Url);
+                    }
+                }
+            }
+        }
+
+
         [HttpCommand("/webapp/config.json")]
         public object LoadParams(HttpRequestParams request)
         {
             return new
             {
-                test = 123
+                map = aliases
             };
         }
     }
