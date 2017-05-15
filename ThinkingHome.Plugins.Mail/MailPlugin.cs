d8a14937d3b4843dcd782db3f96d83575f47f665
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-05-15 22:23:09 +0300

    mail plugin stub

---
+++ ThinkingHome.Plugins.Mail/MailPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.Mail
+{
+    public class MailPlugin : PluginBase
+    {
+        public override void InitPlugin()
+        {
+            base.InitPlugin();
+        }
+    }
+}
