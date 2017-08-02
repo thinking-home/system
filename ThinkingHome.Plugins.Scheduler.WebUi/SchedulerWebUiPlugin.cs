e53bd3124f16460a7e7b80e62a99ddb4b5dc62a2
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-08-02 09:03:05 +0300

    schediler web ui plugin

---
+++ ThinkingHome.Plugins.Scheduler.WebUi/SchedulerWebUiPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.Scheduler.WebUi
+{
+    public class SchedulerWebUiPlugin : PluginBase
+    {
+        public override void InitPlugin()
+        {
+            Logger.LogInformation("mimi");
+        }
+    }
+}
