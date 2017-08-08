b0466ce4dddaef609f658e127426ac203e1cf7d9
Little Cow <lcow90984@gmail.com>
2017-08-08 17:41:53 +0300

    Плагин для отправки/получения MQTT сообщений

---
+++ ThinkingHome.Plugins.Mqtt/MqttPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.Mqtt
+{
+    public class MqttPlugin : PluginBase
+    {
+        public override void InitPlugin()
+        {
+            Logger.LogInformation("init mqtt");
+        }
+    }
+}
