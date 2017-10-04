7b9df450f683ff5486a7a29ed9f8de2441446f4b
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-10-04 20:49:55 +0300

    signalr reconnection & disposing

--- ThinkingHome.Plugins.WebUi/Resources/Application/radio.js
+++ ThinkingHome.Plugins.WebUi/Resources/Application/radio.js
@@ -1,43 +1,45 @@
 var lib = require('lib');
 
 var Radio = lib.common.ApplicationBlock.extend({
+    msgHubUrl: '/mq',
     msgEventName: 'serverMessage',
     reconnectionTimeout: 7000,
 
-    initialize: function () {
-        this.connection = new lib.signalrClient.HubConnection('/mq');
-
-        this.connection.on(this.msgEventName, function (data) {
-            console.log(data);
-        });
-
-        // this.connection = lib.$.hubConnection();
-        // this.connection.disconnected(this.bind('onDisconnect'));
-        //
-        // this.hub = this.connection.createHubProxy(this.msgHubName);
-        // this.hub.on(this.msgEventName, this.bind('onMessage'));
-    },
+    // initialize: function () {
+    // },
 
     start: function () {
-        this.connection.start();
+        this.openConnection();
     },
-    // onBeforeDestroy: function () {
-    //     var connection = this.connection;
-    //     delete this.connection;
-    //
-    //     connection && connection.stop();
-    // },
-    // onDisconnect: function () {
-    //     this.connection && setTimeout(this.bind(function () {
-    //         var connection = this.connection;
-    //         connection && connection.start();
-    //     }), this.reconnectionTimeout);
-    // },
-    // onMessage: function (message) {
-    //     this.trigger(message.channel, message);
-    // },
+
+    onBeforeDestroy: function () {
+        var connection = this.connection;
+        delete this.connection;
+
+        connection && connection.stop();
+    },
+
+    openConnection: function () {
+        var connection = this.connection = new lib.signalrClient.HubConnection(this.msgHubUrl);
+
+        connection.on(this.msgEventName, this.bind('onMessage'));
+        connection.onClosed = this.bind('onDisconnect');
+
+        connection.start().catch(this.bind('onDisconnect'));
+    },
+
+    onDisconnect: function () {
+        if (this.connection) {
+            setTimeout(this.bind("openConnection"), this.reconnectionTimeout);
+        }
+    },
+
+    onMessage: function (message) {
+        this.trigger(message.channel, message);
+    },
+
     sendMessage: function (channel, data) {
-        this.connection.invoke(this.msgEventName, channel, data);
+        this.connection && this.connection.invoke(this.msgEventName, channel, data);
     }
 });
 
