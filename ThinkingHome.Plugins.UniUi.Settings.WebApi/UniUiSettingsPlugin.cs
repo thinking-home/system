36016560b496086c9f300ee2eadd7e3008c7438f
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-01-27 13:44:04 +0300

    uniui settings web api project

---
+++ ThinkingHome.Plugins.UniUi.Settings.WebApi/UniUiSettingsPlugin.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using ThinkingHome.Core.Plugins;
+using ThinkingHome.Plugins.Database;
+using ThinkingHome.Plugins.UniUi.Model;
+using ThinkingHome.Plugins.WebServer.Attributes;
+using ThinkingHome.Plugins.WebServer.Handlers;
+
+namespace ThinkingHome.Plugins.UniUi.Settings.WebApi
+{
+    public class UniUiSettingsPlugin : PluginBase
+    {
+        private object ToDashboardApiModel(Dashboard dashboard)
+        {
+            return new
+            {
+                id = dashboard.Id,
+                title = dashboard.Title,
+                sortOrder = dashboard.SortOrder
+            };
+        }
+
+        [WebApiMethod("/api/uniui/settings/web-api/dashboard/list")]
+        public object GetDashboardList(HttpRequestParams request)
+        {
+            using (var session = Context.Require<DatabasePlugin>().OpenSession())
+            {
+                var list = session.Set<Dashboard>()
+                    .OrderBy(d => d.SortOrder)
+                    .Select(ToDashboardApiModel)
+                    .ToArray();
+
+                return list;
+            }
+        }
+    }
+}
--- ThinkingHome.Plugins.UniUi/UniUiPlugin.cs
+++ ThinkingHome.Plugins.UniUi/UniUiPlugin.cs
@@ -1,9 +1,19 @@
-﻿using ThinkingHome.Core.Plugins;
+﻿using Microsoft.EntityFrameworkCore;
+using ThinkingHome.Core.Plugins;
+using ThinkingHome.Plugins.Database;
+using ThinkingHome.Plugins.UniUi.Model;
 
 namespace ThinkingHome.Plugins.UniUi
 {
     public class UniUiPlugin : PluginBase
     {
-
+        [DbModelBuilder]
+        public void InitModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dashboard>(cfg => cfg.ToTable("UniUi_Dashboard"));
+            modelBuilder.Entity<Panel>(cfg => cfg.ToTable("UniUi_Panel"));
+            modelBuilder.Entity<Widget>(cfg => cfg.ToTable("UniUi_Widget"));
+            modelBuilder.Entity<WidgetParameter>(cfg => cfg.ToTable("UniUi_WidgetParameter"));
+        }
     }
 }
