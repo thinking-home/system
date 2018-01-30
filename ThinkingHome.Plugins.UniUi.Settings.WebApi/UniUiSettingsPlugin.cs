98deb449241690fd9f36d39b61a7eb507d57f918
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-01-30 09:39:42 +0300

    save/delete dashboard

--- ThinkingHome.Plugins.UniUi.Settings.WebApi/UniUiSettingsPlugin.cs
+++ ThinkingHome.Plugins.UniUi.Settings.WebApi/UniUiSettingsPlugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using ThinkingHome.Core.Plugins;
 using ThinkingHome.Plugins.Database;
 using ThinkingHome.Plugins.UniUi.Model;
@@ -31,6 +32,49 @@ namespace ThinkingHome.Plugins.UniUi.Settings.WebApi
 
                 return list;
             }
+        }
+
+        [WebApiMethod("/api/uniui/settings/web-api/dashboard/save")]
+        public object CreateDashboard(HttpRequestParams request)
+        {
+            var id = request.GetGuid("id");
+            var title = request.GetRequiredString("title");
+
+            using (var session = Context.Require<DatabasePlugin>().OpenSession())
+            {
+                Dashboard dashboard;
+
+                if (id.HasValue)
+                {
+                    dashboard = session.Set<Dashboard>().Single(s => s.Id == id.Value);
+                }
+                else
+                {
+                    dashboard = new Dashboard { Id = Guid.NewGuid(), SortOrder = int.MaxValue };
+                    session.Set<Dashboard>().Add(dashboard);
+                }
+
+                dashboard.Title = title;
+                session.SaveChanges();
+
+                return dashboard.Id;
+            }
+        }
+
+        [WebApiMethod("/api/uniui/settings/web-api/dashboard/delete")]
+        public object DeleteDashboard(HttpRequestParams request)
+        {
+            var id = request.GetRequiredGuid("id");
+
+            using (var session = Context.Require<DatabasePlugin>().OpenSession())
+            {
+                var dashboard = session.Set<Dashboard>().Single(s => s.Id == id);
+
+                session.Set<Dashboard>().Remove(dashboard);
+                session.SaveChanges();
+            }
+
+            return null;
         }
     }
 }
