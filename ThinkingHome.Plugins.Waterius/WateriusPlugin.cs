64f19d7f01e2657acb4acdacbaa839178381face
postcretinism <postcretinism@gmail.com>
2025-01-17 21:32:07 +0300

    добавили новый проект с плагином для ватериуса добавили для него конфигурацию

---
+++ ThinkingHome.Plugins.Waterius/WateriusPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.Waterius;
+
+public class WateriusPlugin : PluginBase {
+    public string? MqttTopic => Configuration["mqttTopic"];
+
+    public override void InitPlugin()
+    {
+        if (string.IsNullOrWhiteSpace(MqttTopic)) {
+            Logger.LogWarning("MQTT Topic is undefined");
+        }
+        else {
+            Logger.LogInformation("Listen to waterius in topic: {MqttTopic}...", MqttTopic);
+        }
+    }
+}
