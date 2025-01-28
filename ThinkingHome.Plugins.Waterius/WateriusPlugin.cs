630790c9feb94e61bccf32ce79cf9e612ec0ff9b
postcretinism <postcretinism@gmail.com>
2025-01-29 00:04:40 +0300

    сделали подписку на сообщения от ватериуса по протоколу мктт

--- ThinkingHome.Plugins.Waterius/WateriusPlugin.cs
+++ ThinkingHome.Plugins.Waterius/WateriusPlugin.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
 using ThinkingHome.Core.Plugins;
 using ThinkingHome.Plugins.Mqtt;
+using ThinkingHome.Plugins.Mqtt.DynamicConfiguration;
 
 namespace ThinkingHome.Plugins.Waterius;
 
 public class WateriusPlugin : PluginBase {
+
+    [ConfigureMqtt]
+    public void WateriusListeners(MqttConfigurationBuilder config)
+    {
+        config.RegisterListener(MqttTopic?.Trim('/') + "/#", HandleMqttMessage);
+    }
+
+    private void HandleMqttMessage(string topic, byte[] payload)
+    {
+        var str = Encoding.UTF8.GetString(payload);
+
+        Logger.LogInformation($"{topic}: {str}");
+    }
     public string? MqttTopic => Configuration["mqttTopic"];
     
     private readonly MqttPlugin mqttPlugin;
