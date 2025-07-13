176ddec4379abcbef18e6a55f6af61cc43e867c6
postcretinism <postcretinism@gmail.com>
2025-07-14 00:12:25 +0300

    созал новый плагин исправил документацию

---
+++ ThinkingHome.Plugins.TelegramChatList.WebUi/TelegramChatListWebUiPlugin.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.TelegramChatList.WebUi;
+
+public class TelegramChatListWebUiPlugin : PluginBase {
+    public override void StartPlugin()
+    {
+        Logger.LogInformation("куку ахахахахахаха");
+    }
+}
